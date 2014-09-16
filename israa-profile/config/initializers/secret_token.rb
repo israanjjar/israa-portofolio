523aea56a5a7c4fbacc28c62c516654ee13a8a32
Israa Elnjjar <israa@beheld.me>
2014-09-16 09:59:42 -0700

    setup the project, created a new rails app, added bootstrap and AngularJS and tested that both are working correctly, the stup was successful.. next step would be the design then implemnting the design, lastly I will add examples of my work and finish the portfolio

---
+++ israa-profile/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IsraaProfile::Application.config.secret_key_base = '655124883df39d6afcf91532b49699b7b1dba9f1f6936ebfa87b8e287638c8e395b7390ed338a8d6c3fd653a8e2dfc3d03e65fe30083e9af507b682143b18d5c'
---
+++ israa-profile/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IsraaProfile::Application.config.session_store :cookie_store, key: '_israa-profile_session'
